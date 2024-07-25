a9f271879f4c0aee1196c51c55d43e5ed6a13c4c
Jilmar01 <jilmar657@hotmail.com>
2024-07-24 23:27:14 -0500

    Nuevos Datos

--- src/main/java/com/mycompany/p2lab2tddgonzalezjilmarlaraanderson/P2Lab2TDDGonzalezJilmarLaraAnderson.java
+++ src/main/java/com/mycompany/p2lab2tddgonzalezjilmarlaraanderson/P2Lab2TDDGonzalezJilmarLaraAnderson.java
@@ -6,6 +6,8 @@ public class P2Lab2TDDGonzalezJilmarLaraAnderson {
     public static void main(String[] args) {
 
         System.out.println("\nUNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE");
+        System.out.println("Integrantes: Jilmar Gonzalez");
+        System.out.println("             Anderson Lara");
 
     }
 }
