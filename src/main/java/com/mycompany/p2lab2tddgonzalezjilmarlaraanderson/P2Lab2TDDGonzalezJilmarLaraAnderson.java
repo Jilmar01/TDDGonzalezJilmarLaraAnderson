27fc1585ed8b8a0c95fc1e0213bf0b6b8edbd9a5
Jilmar01 <jilmar657@hotmail.com>
2024-07-24 23:14:33 -0500

    Version 1

---
+++ src/main/java/com/mycompany/p2lab2tddgonzalezjilmarlaraanderson/P2Lab2TDDGonzalezJilmarLaraAnderson.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.p2lab2tddgonzalezjilmarlaraanderson;
+
+public class P2Lab2TDDGonzalezJilmarLaraAnderson {
+
+    public static void main(String[] args) {
+
+        System.out.println("\nUNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE");
+
+    }
+}
